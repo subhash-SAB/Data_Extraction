a99105db2f9378caeb7974834b41d534d77c6fd4
subhash-SAB <subhasishbisoi289@gmail.com>
2026-06-27 09:57:17 +0530

    Initial upload

---
+++ CORE/registry.py
@@ -0,0 +1,15 @@
+from plugins.base import BaseExtractor
+
+class PluginRegistry:
+ 
+   def _init_(self):
+      self._plugin: list[BaseExtractor]=[]
+
+   def register(self, plugin: BaseExtractor):
+      self._plugin.append(plugin)
+
+   def get_plugin(self) -> list[BaseExtractor]:
+      return self._plugin
+   def clear(self):  
+      self._plugin.clear()
+ 
